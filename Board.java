754bd17c97ff070413d9925fa5abf6bf7f739f4a
xu-ji <xj1112@ic.ac.uk>
2012-11-28 19:28:49 +0000

    BOARD AND PLAYER COMPLETED

--- Board.java
+++ Board.java
@@ -1,6 +1,146 @@
+import java.util.*;
 class Board {
 
-	public final rows, columngoalpos;
-	public final
+	public final int rows, cols;
+	public int goalPos;
+	public final Map<Integer, Integer> teleMap;
+
+    public Board(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.goalPos = rows*cols -1;
+        this.teleMap = new HashMap<Integer, Integer>();
+    }
+    public static void main(String[] args) {
+        Board test = buildBoard();
+    }
+    public void printBoard(Player[] players) {
+        int digit = 0;
+        for(int size=goalPos;size>0;size/=10)
+            digit ++;
+        for(int j=0;j<=cols*(digit+4)+2;j++)
+            System.out.print("$");
+        System.out.println();
+        System.out.print("$");
+        for(int j=0;j<cols;j++) {
+            System.out.print("+");
+            for(int k=0;k<digit+3;k++)
+                System.out.print("-");
+        }
+        System.out.println("+$");
+        for(int i=rows-1;i>=0;i--) {
+
+            System.out.print("$|");
+            for(int j=0;j<cols;j++) {
+                int curPos;
+                if(i%2==0)
+                    curPos = i*cols + j;
+                else
+                    curPos = i*cols + (cols-1)-j;
+                System.out.print(String.format("%0"+digit+"d",curPos)+"   |");
+            }
+            System.out.println("$");
+
+            System.out.print("$|");
+            for(int j=0;j<cols;j++) {
+                int curPos;
+                if(i%2==0)
+                    curPos = i*cols + j;
+                else
+                    curPos = i*cols + (cols-1)-j;
+                char symbol = ' ';
+                for(int k=0;k<players.length;k++)
+                    if(players[k].position == curPos)
+                        switch(symbol) {
+                            case ' ': symbol = players[i].symbol;
+                                      break;
+                            default : symbol = '*';
+                                      break;
+                        }
+                if(!teleMap.containsKey(curPos)) {
+                    for(int k=0;k<digit;k++)
+                        System.out.print(" ");
+                    System.out.print(" "+symbol+" |");
+                } else {
+                    curPos = teleMap.get(curPos);
+                    System.out.print(String.format("%0"+digit+"d",curPos)+"   |");
+                }
+            }
+            System.out.println("$");
+
+            System.out.print("$");
+            for(int j=0;j<cols;j++) {
+                System.out.print("+");
+                for(int k=0;k<digit+3;k++)
+                    System.out.print("-");
+            }
+            System.out.println("+$");
+        }
+        for(int j=0;j<=cols*(digit+4)+2;j++)
+            System.out.print("$");
+        System.out.println();
+    }
+    public static Board buildBoard() {
+        System.out.print("How many rows? :");
+        int rows = IOUtil.readInt();
+        System.out.print("How many columns? :");
+        int cols = IOUtil.readInt();
+        
+        Board board = new Board(rows, cols);
+       
+        Player[] emptyPlayers = new Player[0];
+        while (true) {
+            board.printBoard(emptyPlayers);
+            System.out.println("You now have "+board.teleMap.size()+
+                                    " teleport(s).");
+            System.out.println("Say  add     to add another teleport;");
+            System.out.println("     delete  to delete an existing teleport;");
+            System.out.println("     finish  to begin the game.");
+            String cmd = IOUtil.readString();
+            if (cmd.equals("add")) {
+                int source;
+                int dest;
+                while (true) {
+                    System.out.print("Please enter the location of the teleport: ");
+                    source = IOUtil.readInt();
+                    System.out.print("Please enter the destination of the teleport: ");
+                    dest = IOUtil.readInt();
+                    if ( 0 <= source && source <= board.goalPos
+                        &&  0 <= dest    && dest   <= board.goalPos
+                        && !board.teleMap.containsKey(source)
+                        && source!=dest) {
+                        break;
+                    }
+                    System.out.println("STOP TROLLING ME.");
+                }
+                board.teleMap.put(source,dest);
+            }
+            else if (cmd.equals("delete")) {
+                int delKey;
+                while (true) {
+                    System.out.print("Please enter the location of the teleport you want to delete: ");
+                    delKey = IOUtil.readInt();
+                    if (0 <= delKey && delKey <= board.goalPos)
+                        break;
+                    System.out.println("Please enter a valid key!");
+                }
+                if(board.teleMap.remove(delKey)!=null)
+                    System.out.println("Delete successful");
+                else
+                    System.out.println("Delete unsuccessful");
+            }
+            else if (cmd.equals("finish")) {
+                break;
+            }
+            else 
+                System.out.println("Wrong syntax. Try again.");
+        }
+
+        
+        
+        
+        return board;
+    }
+
 
 }
--- Player.java
+++ Player.java
@@ -7,10 +7,46 @@ class Player {
 	public Player(String name,char symbol) {
 		this.name = name;
 		this.symbol = symbol;
+		position = 0;
 	}
 
-	public move(Board board,int numSteps) {
-		if(position+numStep > go)
+	public void move(Board board,int numSteps) {
+		if(position+numSteps <= board.goalPos)
+			position += numSteps;
+        else
+            position = 2*board.goalPos - (position+numSteps);
 	}
+
+    public boolean teleport(Board board) {
+        if (!board.teleMap.containsKey(position))
+            return false;
+        position = board.teleMap.get(position);
+        return true;
+    }
+
+    public static Player[] getPlayers() {
+        System.out.print("How many are you? : ");
+        int n = IOUtil.readInt();
+
+        Player[] players = new Player[n];
+        for (int i = 0; i<n; i++) {
+            System.out.print("Player " + (i+1) + "please enter your name : ");
+            String name = IOUtil.readString();
+            System.out.print("Player " + name + "please enter your chosen symbol : ");  
+	    String symbol;
+            while (true) {
+                symbol = IOUtil.readString();
+                if (symbol.length() == 1) {
+                    break;
+                }
+                System.out.print("Please give a proper symbol! Try again : ");
+            }
+            
+            players[i] = new Player(name, symbol.charAt(0));
+        }
+        return players;
+    }
+
+
 
 }
